c9fc807ef64cc3c71663ee1fe36b551f8e0f7606
Haley Proctor <hproctor021@gmail.com>
2020-07-29 16:53:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,31 @@
+
+$phrase = "It's-a me, Mario!"
+$status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
+$says = "It's Dangerous To Go Alone! Take This."
+  
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
+end
+
+def link
+  puts $says
+end
+
+def all_phrases(link)
+  case character
+  
+  when mario
+    puts $phrase
+  when toadstool
+    puts $status
+  when link
+    puts $says
+  else
+    puts "Where did everyone go?"
+end
 end
